965c263b6d4f99b0fa0485b8afc88e19d2fa79e6
Razvan Dumitrache <razvan.d@codingheads.com>
2024-12-20 01:00:51 +0200

    fix

--- src/Pages/CreateTrip/CreateTrip.tsx
+++ src/Pages/CreateTrip/CreateTrip.tsx
@@ -1,28 +1,18 @@
 import React, { useState } from 'react';
 import './CreateTrip.css';
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 import DatePicker from 'react-datepicker';
 import { useCreateTripMutation } from '../../features/api';
 
-const CreateTrip = ({ user }) => {
+const CreateTrip = () => {
   const [tripName, setTripName] = useState('');
   const [description, setDescription] = useState('');
-  const [participants, setParticipants] = useState([]);
-  const [startDate, setStartDate] = useState<Date | null>(null);
-  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [startDate, setStartDate] = useState<Date>();
+  const [endDate, setEndDate] = useState<Date>();
 
   const [createTrip] = useCreateTripMutation();
 
-  //const [newParticipant, setNewParticipant] = useState('');
-
-  // const handleAddParticipant = () => {
-  //   if (newParticipant.trim()) {
-  //     setParticipants([...participants, newParticipant.trim()]);
-  //     setNewParticipant('');
-  //   }
-  // };
-
-  const handleDateChange = (dates) => {
+  const handleDateChange = dates => {
     const [start, end] = dates;
     setStartDate(start);
     setEndDate(end);
@@ -34,7 +24,7 @@ const CreateTrip = ({ user }) => {
       destination: tripName,
       description: description,
       startDate: startDate?.toISOString(),
-      endDate: endDate?.toISOString()
+      endDate: endDate?.toISOString(),
       //participants: participants,
     };
 
@@ -45,68 +35,59 @@ const CreateTrip = ({ user }) => {
     }
 
     alert('Trip created successfully');
-
   };
 
   return (
-    <div className="createTrip">
-      <h1 className="title">Create Trip</h1>
+    <div className='createTrip'>
+      <h1 className='title'>Create Trip</h1>
 
       <div>
         <h2>Destination</h2>
         <input
-          type="text"
+          type='text'
           value={tripName}
-          onChange={(e) => setTripName(e.target.value)}
-          placeholder="Enter destination name"
+          onChange={e => setTripName(e.target.value)}
+          placeholder='Enter destination name'
         />
       </div>
 
       <div>
         <h2>Description</h2>
         <input
-          type="text"
+          type='text'
           value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="Enter a description"
+          onChange={e => setDescription(e.target.value)}
+          placeholder='Enter a description'
         />
       </div>
 
-      {/* <div>
-        <h2>Participants</h2>
-        <input
-          type="text"
-          value={newParticipant}
-          onChange={(e) => setNewParticipant(e.target.value)}
-          placeholder="Add participant"
-        />
-        <button onClick={handleAddParticipant}>Add</button>
-        <ul>
-          {participants.map((participant, index) => (
-            <li key={index}>{participant}</li>
-          ))}
-        </ul>
-      </div> */}
-
       <div>
         <div>
-          <label htmlFor="start-date">Start Date:</label>
+          <label htmlFor='start-date'>Start Date:</label>
           <input
-            type="text"
-            id="start-date"
-            value={startDate ? new Date(startDate.getTime() - startDate.getTimezoneOffset() * 60000).toISOString().split('T')[0] : ''}
+            type='text'
+            id='start-date'
+            value={
+              startDate
+                ? new Date(startDate.getTime() - startDate.getTimezoneOffset() * 60000).toISOString().split('T')[0]
+                : ''
+            }
             readOnly
-            placeholder="Select a start date"
+            placeholder='Select a start date'
           />
         </div>
         <div>
-          <label htmlFor="end-date">End Date:</label>
+          <label htmlFor='end-date'>End Date:</label>
           <input
-            type="text"
-            id="end-date"
-            value={endDate ? new Date(endDate.getTime() - endDate.getTimezoneOffset() * 60000).toISOString().split('T')[0] : ''}
+            type='text'
+            id='end-date'
+            value={
+              endDate
+                ? new Date(endDate.getTime() - endDate.getTimezoneOffset() * 60000).toISOString().split('T')[0]
+                : ''
+            }
             readOnly
-            placeholder="Select an end date"
+            placeholder='Select an end date'
           />
         </div>
         <DatePicker
@@ -119,23 +100,17 @@ const CreateTrip = ({ user }) => {
           minDate={new Date()}
           showMonthDropdown
           showYearDropdown
-          dropdownMode="select"
+          dropdownMode='select'
           renderCustomHeader={({ date, changeYear, changeMonth }) => (
             <div>
-              <select
-                value={date.getFullYear()}
-                onChange={({ target: { value } }) => changeYear(Number(value))}
-              >
+              <select value={date.getFullYear()} onChange={({ target: { value } }) => changeYear(Number(value))}>
                 {Array.from({ length: 101 }, (_, i) => new Date().getFullYear() - 50 + i).map(year => (
                   <option key={year} value={year}>
                     {year}
                   </option>
                 ))}
               </select>
-              <select
-                value={date.getMonth()}
-                onChange={({ target: { value } }) => changeMonth(Number(value))}
-              >
+              <select value={date.getMonth()} onChange={({ target: { value } }) => changeMonth(Number(value))}>
                 {[
                   'January',
                   'February',
@@ -160,7 +135,7 @@ const CreateTrip = ({ user }) => {
         />
       </div>
 
-      <button onClick={handleSaveTrip} className="save-trip-btn">
+      <button onClick={handleSaveTrip} className='save-trip-btn'>
         Save Trip
       </button>
     </div>
